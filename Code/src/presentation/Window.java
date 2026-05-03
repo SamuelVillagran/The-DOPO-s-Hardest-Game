9028edb76f5c67812acc44efb5ef8e0501fdf552
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-05-02 20:48:16 -0500

    Window con  barra de menú sin funcionalidades

--- Code/src/presentation/Window.java
+++ Code/src/presentation/Window.java
@@ -1,33 +1,77 @@
 package presentation;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class Window {
+public class Window extends JFrame {
 
 	private static JFrame window; 
+	private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem optionOpen, optionSaveAs, optionImport, optionExportAs, optionNew, optionExit;
 	
-	public static void main(String[] args) {
+    public Window() {
+    	setScreen();
+    	prepareElements();
+    	prepareActions();
+	}
+	
+    private void prepareActions() {
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void prepareElements() {
+		prepareElementsMenu();
 		
-		window  = new JFrame();
-		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.setTitle("The DOPO Hardest Game");
+	}
+	
+	private void prepareElementsMenu() {
+		menuBar  = new JMenuBar();
+    	menu = new JMenu("Archivo");
+    	optionNew = new JMenuItem("Nuevo");
+    	optionSaveAs = new JMenuItem("Guardar");
+    	optionOpen = new JMenuItem("Abrir");
+    	optionImport = new JMenuItem("Importar");
+    	optionExportAs = new JMenuItem("Exportar como");
+    	optionExit = new JMenuItem("Salir");
+    	
+    	menu.add(optionNew);
+    	menu.addSeparator();
+    	menu.add(optionSaveAs);
+    	menu.add(optionOpen);
+    	menu.addSeparator();
+    	menu.add(optionExportAs);
+    	menu.add(optionImport);
+    	menu.addSeparator();
+    	menu.add(optionExit);
+    	
+    	menuBar.add(menu);
+    	setJMenuBar(menuBar);
+	}
+	
+
+	private void setScreen() {
+		setResizable(false);
+		setTitle("The DOPO Hardest Game");
 		
 		//TheDOPOHardestGameGUI gamePanel = new TheDOPOHardestGameGUI();
 		//window.add(gamePanel);
 		GameContainer gameContainer = new GameContainer();
-		window.add(gameContainer);
+		add(gameContainer);
 		
+		pack();
 		
-		window.pack();
+		setLocationRelativeTo(null);
 		
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
 		
 		gameContainer.getPlayerModePanel().startGameThread();
-		
-		
+    }
+    
+    
+	public static void main(String[] args) {
+		window  = new Window();
+		window.setVisible(true);
 	}
-
 }
