aba660feb8f24f9a7cf8e37e374e936167774c2e
SamuelVillagran <samuel.villagranr@gmail.com>
2026-05-07 21:53:11 -0500

    Agregando clase Zone

--- Code/src/domain/Tile.java
+++ Code/src/domain/Tile.java
@@ -2,6 +2,8 @@ package domain;
 
 public abstract class Tile extends Element {
 
+	
+	
 	public String getNameSuperClass() {
 		return this.getClass().getSuperclass().getSimpleName().toLowerCase();
 	}
--- Code/src/domain/Zone.java
+++ Code/src/domain/Zone.java
@@ -1,9 +1,24 @@
 package domain;
 
-public class Zone {
+import java.awt.Point;
+import java.util.List;
+
+public abstract class Zone {
 
 	private Tile[] tiles;
+	private Figure bounds;
 	
+	//form : _List<Point>
+	/**
+	 * This is the zone Where going to affect the player
+	 * @param form form is the coordinates of tiles to make the zone
+	 */
+	public Zone(List<Point> coordinatesTiles) {
+		bounds = new Figure(coordinatesTiles);
+		for (Point p : coordinatesTiles) {
+			
+		}
+	}
 	
 	
 }
