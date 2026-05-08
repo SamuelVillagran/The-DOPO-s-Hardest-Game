f9923b588f8a284c6e92a142e10d13e8148da183
SamuelVillagran <samuel.villagranr@gmail.com>
2026-05-07 22:01:43 -0500

    Agregando resto de tiles

--- Code/src/domain/DinamicTile.java
+++ Code/src/domain/DinamicTile.java
@@ -1,5 +1,24 @@
 package domain;
 
-public class DinamicTile {
+public abstract class DinamicTile {
 
+	private boolean isActive; 
+	
+	/**
+	 * This make the interactions at the levels with the entities
+	 * @param entity entity that going to interact with everything at the specific level
+	 * @param level level is level that have diferents things to entities interacts
+	 */
+	public void interact(Entity entity, Level level) {
+		
+	}
+	
+	/**
+	 * This method going to do some effect with the specific entity given
+	 * @param entity entity that going to apply some special effect
+	 */
+	private void applyEffect(Entity entity) {
+		
+	}
+	
 }
